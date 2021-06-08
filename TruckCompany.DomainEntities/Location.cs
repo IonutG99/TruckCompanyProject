a2edbd0e569f09bdae77d1f56a70a8f9fd8a3d2f
ionut ghimici <ionutghimici@yahoo.com>
2021-06-09 01:00:21 +0300

    Location update.

--- TruckCompany.DomainEntities/Location.cs
+++ TruckCompany.DomainEntities/Location.cs
@@ -1,7 +1,15 @@
-﻿namespace TruckCompany.DomainEntities
+﻿using System;
+
+namespace TruckCompany.DomainEntities
 {
     public class Location
     {
+        public Location()
+        {
+            Random random = new Random();
+            int nr = random.Next();
+            Id = nr;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
     }
